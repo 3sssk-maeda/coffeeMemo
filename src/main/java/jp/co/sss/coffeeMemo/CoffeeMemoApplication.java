1916fb53c7009642f12fa0ce19dfb60e8785e598
MomokoUchishiba <86950715+MomokoUchishiba@users.noreply.github.com>
2021-10-20 20:53:39 +0900

    STSプロジェクト作成

---
+++ src/main/java/jp/co/sss/coffeeMemo/CoffeeMemoApplication.java
@@ -0,0 +1,13 @@
+package jp.co.sss.coffeeMemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeMemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/sss/coffeeMemo/CoffeeMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.sss.coffeeMemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
